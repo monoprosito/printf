ae62d9d3cab9b6dde108792edd36e5d63f76f746
Santiago Arboleda Londoño <monoprosito@gmail.com>
2019-10-27 22:57:35 +0000

    Declares the structure to the specifiers
    In this commit the library <stdarg.h> is added by
    default and a structure is added to compare the
    format specifiers that are sent as parameter to
    the _printf() function.

--- holberton.h
+++ holberton.h
@@ -1,5 +1,19 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+#include <stdarg.h>
+
+/**
+  * struct specifiers - Struct specifiers
+  * @specifier: The conversion specifier
+  * @f: The function pointer
+  */
+typedef struct specifiers
+{
+	char *specifier;
+	void (*f)(va_list args);
+} spc_dt;
+
 int _write(char c);
 int _printf(const char *format, ...);
+
 #endif /* _HOLBERTON */
