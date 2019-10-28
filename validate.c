b1f7c604277fe13b83a036049a6a104c99311f21
jormao <jormao@gmail.com>
2019-10-28 00:57:06 +0000

    validate char
    Validate if a char in format is correct for printf, if is correct return to the printf function
    else exit whit error 10

---
+++ validate.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _evaluate - evaluate if each character of the string format is correct
+ * @str_format: character
+ *
+ */
+
+
+void _evaluate(char *str_format)
+{
+	char array_type[2] = {'c', 's'};
+	int i = 0, j = 0, flag_a = 0;
+
+	if ((str_format != NULL) && (str_format[i] == '\0'))
+		exit(10);
+	while (str_format[i] != '\0')
+	{
+		if (str_format[i] == '%')
+		{
+			while (array_type[j])
+			{
+				if (str_format[i + 1] == array_type[j])
+					flag_a == 1;
+				j++;
+			}
+			if (flag_a == 0)
+				exit(10);
+			flag_a = 0;
+		}
+		i++;
+	}
+}
