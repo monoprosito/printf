3e2834498e60614a25008a47c805624008ae3f7b
jormao <jormao@gmail.com>
2019-10-28 10:46:04 -0500

    ONLY VALIDATE THE NULL
    We was checking more characters than NULL y we have error en some topics. for example %e.
    Now only validate format = NULL

--- validate.c
+++ validate.c
@@ -10,27 +10,7 @@
  */
 int _validate(const char *str_format)
 {
-	char array_type[2] = {'c', 's'};
-	int i = 0, j = 0, flag_a = 0;
-
-	if ((str_format == NULL) || (str_format[i] == '\0'))
+	if (str_format == NULL)
 		return (-1);
-	while (str_format[i] != '\0')
-	{
-		if (str_format[i] == '%')
-		{
-			while (array_type[j])
-			{
-				if (str_format[i + 1] == array_type[j])
-					flag_a = 1;
-				j++;
-			}
-			if (flag_a == 0)
-				return (-1);
-			flag_a = 0;
-			j = 0;
-		}
-		i++;
-	}
 	return (0);
 }
