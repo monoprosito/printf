8af036dd816248f40947331f2226dc65f346bc9c
Santiago Arboleda L <monoprosito@gmail.com>
2019-10-29 11:47:52 -0500

    Refactor the printing of valid and invalid char
    It distributes in different functions the printing of valid and invalid characters. It also verifies an error case in which a parameter '%' is sent and the next character is null

--- print_format.c
+++ print_format.c
@@ -10,54 +10,87 @@
   */
 int _print_format(const char *format, va_list args)
 {
+	int count = 0, i = 0;
+
+	while (format && format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+
+			while (format[i] == ' ')
+				i++;
+
+			if (format[i] == '%')
+				count += _write(format[i]);
+
+			if (format[i] != 'c' && format[i] != 's' && format[i] != '%')
+			{
+				count = _print_invalid_spec(format[i - 1], format[i], count);
+			}
+			else
+			{
+				count += _print_spec(format[i], args);
+			}
+		}
+		else
+		{
+			count += _write(format[i]);
+		}
+
+		i++;
+	}
+
+	return (count);
+}
+
+/**
+  * _print_spec - Prints a valid specifier
+  * @format: The specifier to prints
+  * @args: A list of variadic arguments
+  *
+  * Return: The length of the specifier
+  */
+int _print_spec(char format, va_list args)
+{
+	int i  = 0, length = 0;
 	spc_dt _types[] = {
 		{"c", _print_a_char},
 		{"s", _print_a_string},
 		{NULL, NULL}
 	};
-	int count = 0, i = 0, c = 0, j = 0;
 
-	while (format && format[i])
+	while (_types[i].specifier)
 	{
-		if (format[i] != '%')
-			count += _write(format[i]);
-		if (format[i] == '%')
-		{
-			if (format[i + 1] == '\0')
-				return (-1);
-			i++;
-			while (format[i] == ' ')
-			{
-				i++;
-			}
-			if (format[i] == '%')
-				count += _write(format[i]);
-			if (format[i] != 'c' && format[i] != 's' && format[i] != '%')
-			{
-				count += _write('%');
-				if (format[i - 1] == ' ')
-				{
-					count += _write(' ');
-					count += _write(format[i]);
-				}
-				else
-					count += _write(format[i]);
-			}
-			else
-			{
-				while (_types[j].specifier)
-				{
-					if (*_types[j].specifier == format[i])
-					{
-						c = _types[j].f(args);
-						count += c;
-					}
-					j++;
-				}
-				j = 0;
-			}
-		}
+		if (*_types[i].specifier == format)
+			length = _types[i].f(args);
+
 		i++;
+	}
+
+	return (length);
+}
+
+/**
+  * _print_invalid_spec - Prints a invalid specifier
+  * @prev_format: The previous specifier of actual specifier
+  * @format: The specifier to prints
+  * @count: The current count before prints invalid specifiers
+  *
+  * Return: The current count after prints invalid specifiers
+  */
+int _print_invalid_spec(char prev_format, char format, int count)
+{
+	count += _write('%');
+
+	if (prev_format == ' ')
+	{
+		count += _write(' ');
+		count += _write(format);
+	}
+	else
+	{
+		count += _write(format);
 	}
 
 	return (count);
--- validate.c
+++ validate.c
@@ -10,7 +10,18 @@
  */
 int _validate(const char *str_format)
 {
+	int i = 0;
+
 	if (str_format == NULL)
 		return (-1);
-	return (0);
+
+	while (str_format[i])
+	{
+		if (str_format[i] == '%' && str_format[i + 1] == '\0')
+			return (-1);
+
+		i++;
+	}
+
+	return (1);
 }
